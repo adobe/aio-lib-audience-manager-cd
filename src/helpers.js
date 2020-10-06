c0435ff5dffa06626d2d09a83aa9a67c562e149e
Moritz Raho <raho@adobe.com>
2020-10-06 12:10:37 +0200

    trivial: fix linter typo

--- src/helpers.js
+++ src/helpers.js
@@ -9,8 +9,6 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-/* global Request,  */ // for linter
-
 /**
  * Reduce an Error to a string
  *
